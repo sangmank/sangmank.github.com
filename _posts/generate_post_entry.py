b469fd673810c3cfc66e1b33423666465777d572
Sangman Kim <sangmank@cs.utexas.edu>
2011-05-03 19:15:14 -0500

    generate_post_entry.py added

---
+++ _posts/generate_post_entry.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+import sys, os, re
+import datetime 
+
+orig_title = raw_input("what is the title? (only alphabet or numbers or spaces)")
+
+title = orig_title.replace(" ", "-")
+
+title = re.sub("[^a-zA-Z0-9 -]", "-", title)
+
+today = datetime.date.today()
+
+filename = "%s-%s.textile"%(today.strftime("%Y-%m-%d"), title)
+
+if filename in os.listdir("."):
+	print "file %s already exists"%filename
+	sys.exit(1)
+
+entryf = open(filename, "w")
+entryf.write("""---
+layout: post
+title: %s
+---
+
+h1. {{ page.title }}"""%orig_title)
+entryf.close()
+
+print "file %s has been created" % filename
